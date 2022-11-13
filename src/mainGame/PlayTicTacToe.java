56c213679237837e2e2f7e23dc79f7d14c86ea20
Zebulacious <markwarren@valdosta.edu>
2022-11-12 19:45:10 -0500

    First Commit

---
+++ src/mainGame/PlayTicTacToe.java
@@ -0,0 +1,14 @@
+package mainGame;
+
+public class PlayTicTacToe {
+
+	public static void main(String[] args) {
+		playGame();
+
+	}
+	
+	public static void playGame() {
+		System.out.print("Hello");
+	}
+
+}
