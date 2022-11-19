724294eeff3f03329657285e8f7277071843ea86
Mark <mark16810@gmail.com>
2022-11-18 20:23:08 -0500

    added recursion and move selection

--- src/mainGame/Board.java
+++ src/mainGame/Board.java
@@ -1,41 +1,38 @@
 package mainGame;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Board {
-	private Character[] board = new Character[9];
+	private Character[] board = {'_', '_', '_', '_', '_', '_', '_', '_', '_'};
 	private int turn = 0;
-	private Character turnCharacter = 'O';
+	private Character turnCharacter = 'Z';
+	private Character ArtificalTC = 'Z';
 //	player is x and x is always turn 1
 	
 	public Board(int t, Character c) {
 		this.turn = t;
 		this.turnCharacter = c;
+		this.ArtificalTC = c;
 		
 	}
 	
-	public int moveCheck() { //should only be for player
-		Scanner scnr = new Scanner(System.in);
-		boolean acceptInput = false;
-		int index = -1;
-		while(acceptInput == false) {
-			index = scnr.nextInt();
-			index = index - 1;
-			try {
-				index = index - 1;
-				if(index > 8 || index < 0) {
-					if(board[index] == 'X' || board[index] == 'O') {
-						acceptInput = true;
-					}
+	public int inputForMove(Scanner c) {
+		if(c.hasNextInt()) {
+			return c.nextInt();
+		}
+		else {
+			System.out.print("Not a valid input");
+			return -10;
+		}
+	}
+	
+	public boolean moveCheck(int move) { //should only be for player
+			if(move < 9 || move >= 0) {
+				if(board[move -1].equals('_')) {
+					return true;
 				}
 			}
-			catch(Exception e) {
-				System.out.print("How hard can it be... 1-9");
-			}
-		}
-		scnr.close();
-		return index;
+		return false;
 	}
 	
 	public void move(int move) {
@@ -44,13 +41,15 @@ public class Board {
 	
 	//computer main
 	public int bestMove() {
-		Character[] tempBoard = getBoard().clone();
 		int bestWeight = Integer.MIN_VALUE;
 		int moveIndex = Integer.MIN_VALUE;
 		
-		for(int i = 0; i < tempBoard.length; i++) {
-			if(tempBoard[i].equals(null)) { //for all the empty spots left in the game we will call the recursive weighting function
-				int currentWeight = findWeight(tempBoard, i);
+		for(int i = 0; i < board.length; i++) {
+			if(board[i].equals('_')) { //for all the empty spots left in the game we will call the recursive weighting function
+				board[i] = 'O';
+				updateArtificalTC();
+				int currentWeight = findWeight(board, false); //for any given move of the computer, what is the boardest one?
+				board[i] = '_';
 				if(currentWeight > bestWeight) {
 					bestWeight = currentWeight;
 					moveIndex = i;
@@ -62,50 +61,113 @@ public class Board {
 	}
 	
 	//recursive call
-	public int findWeight(Character[] b, int moveSpace) {
-		Character t = getTurnCharacter();
-		b[moveSpace] = t;
-		updateTurn();
-		int weight = 0;
-		
+	public int findWeight(Character[] b, boolean isMax) {
 		//base cases
-		if(b[0].equals('t') && b[1].equals('t') && b[2].equals('t')) {
-			return 1;
+		if(checkTie(b) == true) {
+			return 0;
 		}
-		if(b[3].equals('t') && b[4].equals('t') && b[5].equals('t')) {
-			return 1;
+		if(oWin(b) == true) { //win for Computer
+			return 15;
 		}
-		if(b[6].equals('t') && b[7].equals('t') && b[8].equals('t')) {
-			return 1;
-		}
-		if(b[0].equals('t') && b[3].equals('t') && b[6].equals('t')) {
-			return 1;
-		}
-		if(b[1].equals('t') && b[4].equals('t') && b[7].equals('t')) {
-			return 1;
-		}
-		if(b[2].equals('t') && b[5].equals('t') && b[8].equals('t')) {
-			return 1;
-		}
-		if(b[0].equals('t') && b[4].equals('t') && b[8].equals('t')) {
-			return 1;
-		}
-		if(b[2].equals('t') && b[4].equals('t') && b[6].equals('t')) {
-			return 1;
+		if(xWin(b) == true) { //win for Computer
+			return -15;
 		}
 		
-		for(int i = 0; i < b.length; i++) {
-			if(b[i].equals(null)) {
-				weight += findWeight(b, i);
-			}
-		}
-		return weight;
-		
+		//recursive
+		//  Computer, find the maximum val.
+        if (isMax) {
+            int highestVal = Integer.MIN_VALUE;
+            for (int index = 0; index < b.length; index++) {
+            	if(b[index] == '_') {
+                board[index] = 'O';
+                highestVal = Math.max(highestVal, findWeight(board, false));
+                board[index] = '_';
+                }
+            }
+            return highestVal;
+        }
+            // Player, find minimum val;
+        else {
+        	int lowestVal = Integer.MAX_VALUE;
+            for (int index = 0; index < b.length; index++) {
+            	if(b[index] == '_') {
+                board[index] = 'X';
+                lowestVal = Math.min(lowestVal, findWeight(board, true));
+                board[index] = '_';
+                }
+            }
+            return lowestVal;
+        }
 	}
 	
+	//check win
+	public boolean xWin(Character[] b) {
+		if(b[0] == ('X') && b[1] == ('X') && b[2] == ('X')) {
+			return true;
+		}
+		if(b[3] == ('X') && b[4] == ('X') && b[5] == ('X')) {
+			return true;
+		}
+		if(b[6] == ('X') && b[7] == ('X') && b[8] == ('X')) {
+			return true;
+		}
+		if(b[0] == ('X') && b[3] == ('X') && b[6] == ('X')) {
+			return true;
+		}
+		if(b[1] == ('X') && b[4] == ('X') && b[7] == ('X')) {
+			return true;
+		}
+		if(b[2] == ('X') && b[5] == ('X') && b[8] == ('X')) {
+			return true;
+		}
+		if(b[0] == ('X') && b[4] == ('X') && b[8] == ('X')) {
+			return true;
+		}
+		if(b[2] == ('X') && b[4] == ('X') && b[6] == ('X')) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
-	public int getTurn() {
-		return turn;
+	public boolean oWin(Character[] b) {
+		if(b[0] == ('O') && b[1] == ('O') && b[2] == ('O')) {
+			return true;
+		}
+		if(b[3] == ('O') && b[4] == ('O') && b[5] == ('O')) {
+			return true;
+		}
+		if(b[6] == ('O') && b[7] == ('O') && b[8] == ('O')) {
+			return true;
+		}
+		if(b[0] == ('O') && b[3] == ('O') && b[6] == ('O')) {
+			return true;
+		}
+		if(b[1] == ('O') && b[4] == ('O') && b[7] == ('O')) {
+			return true;
+		}
+		if(b[2] == ('O') && b[5] == ('O') && b[8] == ('O')) {
+			return true;
+		}
+		if(b[0] == ('O') && b[4] == ('O') && b[8] == ('O')) {
+			return true;
+		}
+		if(b[2] == ('O') && b[4] == ('O') && b[6] == ('O')) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean checkTie(Character[] b) {
+		for(int i = 0; i < b.length; i++) {
+			if (b[i] == '_') {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	public void updateTurn() {
@@ -119,12 +181,33 @@ public class Board {
 		}
 	}
 	
+	public void updateArtificalTC() {
+		if(getArtificalTC() == 'X') {
+			ArtificalTC = 'O';
+		}
+		else {
+			ArtificalTC = 'X';
+		}
+	}
+	
+	
+	
+	//getters setters and toString
+	
+	public int getTurn() {
+		return turn;
+	}
+	
 	public Character[] getBoard() {
 		return board;
 	}
 	
 	public Character getTurnCharacter() {
 		return turnCharacter;
+	}
+	
+	public Character getArtificalTC() {
+		return ArtificalTC;
 	}
 
 	public void setTurnCharacter(Character turnCharacter) {
@@ -140,9 +223,9 @@ public class Board {
 	public String toString() {
 		String msg = String.format("\n");
 		msg += String.format("|%c|%c|%c|\n", board[0], board[1], board[2]);
-		msg += String.format("------------\n");
+		msg += String.format("--------\n");
 		msg += String.format("|%c|%c|%c|\n", board[3], board[4], board[5]);
-		msg += String.format("------------\n");
+		msg += String.format("--------\n");
 		msg += String.format("|%c|%c|%c|\n", board[6], board[7], board[8]);
 		return msg;
 	}
--- src/mainGame/PlayTicTacToe.java
+++ src/mainGame/PlayTicTacToe.java
@@ -1,15 +1,14 @@
 package mainGame;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PlayTicTacToe {
 
 	public static void main(String[] args) {
-		//playGame();
+		playGame();
 		
 		//Tests
-		testprintEmptyBoard();
+		//testprintEmptyBoard();
 
 	}
 	
@@ -18,6 +17,9 @@ public class PlayTicTacToe {
 		int turn = (int) Math.round(Math.random());
 		Character letter = 'Z';
 		boolean endGame = false;
+		Scanner scnr = new Scanner(System.in);
+		int q = -10;
+		
 		
 		if(turn == 0) {
 			letter = 'O';
@@ -28,47 +30,54 @@ public class PlayTicTacToe {
 		Board b = new Board(turn, letter);
 		
 //		RULES
-		String rules = String.format("Computer will be - O\nPlayter is X\n");
+		String rules = String.format("Computer will be O\nPlayer is X\n");
 		rules += ("Enter your move as a number (1-9)\n");
 		rules += ("The moves correspond left to right on the board ex. 4 would be middle-left");
 		System.out.println(rules);
 		
 //		game
 		while(endGame != true) {
-			b.toString();
+			System.out.print(b.toString());
 			//Turn message
 			b.turnIndicator();
 			
 			if(b.getTurn() == 1) {
-				int playerMove = b.moveCheck();
-				b.move(playerMove);
+				boolean moveIsValid = false;
+				while(moveIsValid == false) {
+					q = b.inputForMove(scnr);
+					if(b.moveCheck(q)) {
+						moveIsValid = true;
+					}
+				}
+				
+				b.move(q - 1); // translates length from 1-9 to 0-8 user to array
 			}
 			else {
 				int computerMove = b.bestMove();
 				b.move(computerMove);
 			}
+			
+			//check for end game
+			if(b.xWin(b.getBoard())) {
+				System.out.printf(b.toString() + "\nI don't know how you managed it but you won...");
+				endGame = true;
+			}
+			if(b.oWin(b.getBoard())) {
+				System.out.printf(b.toString() + "\nYou lost");
+				endGame = true;
+			}
+			if(b.checkTie(b.getBoard()) && endGame == false) {
+				System.out.printf(b.toString() + "\nIt's a tie", b.getTurnCharacter());
+				endGame = true;
+			}
+			
 			b.updateTurn();
 		}
-		
-		
-		
-		
-		//Helper Methods
-		public static void updateTurn() {
-			if(turn == 1) {
-				turn = 0;
-			}
-			else {
-				turn = 1;
-			}
-		}
-		
-		System.out.print("Hello");
 	}
 	
 	public static void testprintEmptyBoard() {
 		System.out.print("---Testing board toString---\n");
-		Board b = new Board(1);
+		Board b = new Board(1, 'c');
 		System.out.print(b);
 	}
 
